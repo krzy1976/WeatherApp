d6e48c6dc14e94a61b51f9be5643b8af68e332be
krzy1976 <krzysztof.wojciech.dziedzic@gmail.com>
2023-01-31 10:18:30 +0100

    seventh version

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,8 @@ public class Main {
         System.out.println();
         System.out.println();
         System.out.println();
+        System.out.println();
+        System.out.println();
     }
 }
 
